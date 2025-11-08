0cf2a8cb3b3da5d04eb2193367552759d9648e7b
Riceball LEE <snowyu.lee@gmail.com>
2025-11-08 11:58:14 +0800

    feat: add findPort func

---
+++ src/find-port.ts
@@ -0,0 +1,36 @@
+import net from 'net'
+
+export async function findPort(port: string|number, portRetryCount = 10): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (port === undefined) {
+      port = 0
+    } else {
+      if (typeof port === 'string') {port = parseInt(port)}
+      if (!(port >= 0)) {port = 0}
+    }
+    const server = net.createServer();
+    server.on('error', (err: any) => {
+      if (err.code === 'EADDRINUSE') {
+        (port as number)++
+        portRetryCount--
+        if (portRetryCount > 0) {
+          server.listen(port)
+          return
+        }
+      }
+      reject(err)
+    })
+    server.on('listening', async () => {
+      const addr = server.address() as net.AddressInfo
+      const result = addr.port;
+      server.close((err: any)=>{
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+    server.listen(port);
+  })
+}
